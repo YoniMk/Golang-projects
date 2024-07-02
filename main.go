be25d4deeec6be836cb94a990d6c92fc8ef36b35
YoniMk <149786960+YoniMk@users.noreply.github.com>
2024-07-02 23:28:51 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,78 +1,31 @@
 package main
 
-import"fmt"
 
 
 /*
 
-Create a project that asks for student's name, age, yearGrouo, gradesAverage
+Build a project that asks the user for positive integer numbers and accumulates them
+on a variable.
+The program will end when the user enters a negative number
 
 */
+import "fmt"
 
-/*
+func main () {
 
-Sruct is the possibilty for us programmers to create our own data type
+var number int = 1
+var sum int
 
-*/
-
-type student struct {
-
-	name string
-	age int
-	yearGroup string
-	gradeAverage float64
-
+for number > 0 {
+	fmt.Println("Please enter a positive number: ")
+	fmt.Scan(&number)
+	if number > 0 {
+		sum += number // sum = sum + number	
+	} else {
+		fmt.Println("The number you entered is negative, the program will finish!")	
+    }
 }
 
-type product struct {
-	id int
-	name string
-	price float64
-	stock int 
-}
-
-type footballPlayer struct {
-	name string
-	team string
-	shirtNumber int
-	marketValue float64
-	goalScored int
-
-}
-
-func main() {
-
-	var student1 student
-
-	student1.name = "Yoni"
-	student1.age = 28
-	student1.yearGroup = "1st grade"
-	student1.gradeAverage = 13.7
-
-	var player footballPlayer
-
-	player.name = "Ronaldinho"
-	player.team = "Barcelona"
-	player.shirtNumber = 10
-	player.marketValue = 200000
-	player.goalScored = 20
-
-	var products = []product{}
-
-	productsCounter := 0
-
-	for i:=0; i< 5; i++ {
-
-		productsCounter += 1
-		products[i].id = productsCounter
-		fmt.Println("Please enter the product name: ")
-		fmt.Scan(&products[i].name)
-		fmt.Println("Please enter the product price: ")
-		fmt.Scan(&products[i].price)
-		fmt.Println("Please enter the product stock: ")
-		fmt.Scan(&products[i].stock)
-	}
-
-
+   fmt.Println("The final sum is:", sum)
 
 }
